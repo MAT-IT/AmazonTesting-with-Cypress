ae4cccf3a60f709b655ac1e740a484c5cebe8db7
MAT <77358430+matzhd@users.noreply.github.com>
2022-07-05 09:24:09 +0100

    tested bottoms links

--- cypress/integration/TestCases/bottomlinks.cy.js
+++ cypress/integration/TestCases/bottomlinks.cy.js
@@ -2,24 +2,54 @@
 
 import HomePage from "../PageObjectRepository/HomePage"
 
-describe("testing bottom links",()=>{
+describe("testing bottom links", () => {
     const homepage = new HomePage()
-    it("bottom links test verify",()=>{
+
+    beforeEach(() => {
         cy.visit("https://www.amazon.com/")
-        homepage
-        .getButtomAdvertisingLink().then(function(e){
-            const advtext = e.text().substring(7,18).toLowerCase()
-            cy.log(advtext)
-       homepage
-       .getButtomAdvertisingLink().click()
-        cy.url().should("contain",advtext)
-
-        })     
-              
-
-        
     })
 
+    it.skip("advert links test verify", () => {
+
+        homepage
+            .getButtomAdvertisingLink().then(function (e) {
+                const advtext = e.text().substring(7, 18).toLowerCase()
+                cy.log(advtext)
+                homepage
+                    .getButtomAdvertisingLink().click()
+                cy.url().should("contain", advtext)
+
+            })
+
+    })
+
+    it.skip("business links test verify", () => {
+
+        homepage
+            .getButtomBusinessLink().then(function (e) {
+                const bustext = e.text().substring(7, 15)
+                cy.log(bustext)
+        homepage
+             .getButtomBusinessLink().click()
+         cy.title().should("contain", bustext)
+
+            })
+            
+    })
+
+    it("box office links test verify", () => {
+
+        homepage
+            .getButtonBoxOfficeLink().then(function (e) {
+                const bustext = e.text().substring(7, 15)
+                cy.log(bustext)
+        homepage
+              .getButtonBoxOfficeLink().click()
+        cy.title().should("contain", bustext)
+
+            })
+            
+    })
     
 
 
